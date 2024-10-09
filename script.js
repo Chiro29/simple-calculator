e5f692091454fcf6dfe776070da392eb8cc8d894
chiro <80062293+Chiro29@users.noreply.github.com>
2024-10-09 15:17:55 +0200

    feat(script): added 4 basic functions for operations

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(n, nx) {
+  return n + nx;
+}
+
+function subtract(n, nx) {
+  return n - nx;
+}
+
+function multiply(n, nx) {
+  return n * nx;
+}
+
+function divide(n, nx) {
+  return n / nx;
+}
