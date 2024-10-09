15e154263fd4cf2592346c85552044c58c1fa08a
chiro <80062293+Chiro29@users.noreply.github.com>
2024-10-09 15:27:59 +0200

    feat(script): added operate function

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let number = 0;
+let nextNumber = 0;
+let operator = "";
+
+function operate(n, nx, o) {
+  if(o === "+")
+    return add(n, nx);
+  if(o === "-")
+    return subtract(n, nx);
+  if(o === "*")
+    return multiply(n, nx);
+  if(o === "/")
+    return divide(n, nx);
+}
+
 function add(n, nx) {
   return n + nx;
 }
